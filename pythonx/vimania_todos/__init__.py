6baa918c5fc2deee6e6d9c28f28582acf1fc1158
sysid <sysid@gmx.de>
2022-12-23 14:13:30 +0100

    Bump version: 1.2.1 → 1.3.0

--- pythonx/vimania_todos/__init__.py
+++ pythonx/vimania_todos/__init__.py
@@ -2,5 +2,5 @@ from vimania_todos.todos.cli import app
 
 # from vimania_todos.vim_.vimania_manager import VimaniaManager
 
-__version__ = "1.2.1"
+__version__ = "1.3.0"
 # __all__ = ["VimaniaManager", "app"]
