7544007d01b2a7eb4dcf33b7037130324413f9a8
sysid <sysid@gmx.de>
2023-02-19 10:14:06 +0100

    rust integration mvp

---
+++ tests/test_rs.py
@@ -0,0 +1,7 @@
+# noinspection PyUnresolvedReferences,PyProtectedMember
+from vimania_todos import _vimania_todos
+
+
+def test_rs():
+    print("xxxxx")
+    print(_vimania_todos.sum_as_string(1, 2))
